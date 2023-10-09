053dce4e8d6daadaac56bfac310522389b028cbd
HelpflCorn <ilyashaban@yahoo.com>
2023-10-10 01:15:21 +0200

    changing visuals + tests

--- to_do_app/todo_list/base/views.py
+++ to_do_app/todo_list/base/views.py
@@ -21,6 +21,10 @@ class Tasklist(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['count'] = context['tasks'].filter(complete=False).count()
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(task_title__contains=search_input)
+        context['search_input'] = search_input
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
