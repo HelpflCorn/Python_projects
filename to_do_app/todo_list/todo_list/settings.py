920bb2c5af8f34046d923bcf6ae6a43939df9245
HelpflCorn <ilyashaban@yahoo.com>
2023-10-22 20:32:17 +0200

    moving to postgresql

--- to_do_app/todo_list/todo_list/settings.py
+++ to_do_app/todo_list/todo_list/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todoapp',
+        'USER': 'illia',
+        'PASSWORD': '44992067',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
